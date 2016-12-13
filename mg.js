777a241498f10ca9d6adb0b330bd0fb4aa8b451e
marcSSS <sss4259@gmail.com>
2016-12-13 19:12:43 +0800

    v.241

--- mg.js
+++ mg.js
@@ -1,14 +1,14 @@
-function main () {
+
+$(document).ready(function () {
 	$('.skillset').hide();
 	$(".skillset").fadeIn(1000);
   $('.projects').hide();
    $(".projects-button").on("click", function () {
 		$(this).next().slideToggle(400);
     $(this).toggleClass("active");
-    $(this).text('Projects Viewed');
+    $(this).text('已查看');
   });
-}
-$(document).ready(main);
+});
 
 $(".b1").html("更新公告");
 $(".b2").css("background-color"," red");
